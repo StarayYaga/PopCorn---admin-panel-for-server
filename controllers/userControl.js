69af503cb464190287de1614f242c54434b4b05d
StarayYaga <slava8186@gmail.com>
2023-04-06 15:35:56 +0400

    add comments

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -5,33 +5,33 @@ const dataInfo = require('./controllers')
 
 
 class userController {
-    async registration (email, password, role){
-        const candidate = await db.find_one(email)
+    async registration (email, password, role){ // регистрация 
+        const candidate = await db.find_one(email) //поиск почты в бд. Если нашли, то выбрасываем ошибку о существующим пользователе
         if (!candidate){
 			throw new Error('Пользователь с такой ошибкой уже существует')
         }
-		const hashPassword = await bcrypt.hash(password, 3)
-		const token = tokenService.generateToken({"email": email})
-		db.create_user(email, hashPassword, token)
+		const hashPassword = await bcrypt.hash(password, 3)  // шифрование пароля
+		const token = tokenService.generateToken({"email": email})  // генерация токена
+		db.create_user(email, hashPassword, token)  // создание записи в бд
 		return (
             { "refresh":token, "email": email, "role": role, "data": Date.now() + 30*24*60*60*1000}
         )
     }
 
-    async check(email, refresh){
-        const result = await db.getToken(email)
-        if (refresh == result){
+    async check(email, refresh){  // проверка авторизации пользователя
+        const result = await db.getToken(email)  // запрашиваем из бд токен по почте
+        if (refresh == result){  // сравниваем
             return true
         }
         return false
     }
 
-    async login(email, password){
-        const dbPassword = await (await db.getHash(email))
-        const hashPassword = await bcrypt.compare(password, dbPassword)
+    async login(email, password){ // авторизация 
+        const dbPassword = await (await db.getHash(email)) // запрашиваем захешированный пароль по почте
+        const hashPassword = await bcrypt.compare(password, dbPassword) // сравниваем их
         if (hashPassword){
-            const token = tokenService.generateToken({"email": email})
-            db.update_token_user(email, token)
+            const token = tokenService.generateToken({"email": email}) // генерация нового токена
+            db.update_token_user(email, token) // изменяем токен в бд
             return (
                 { "refresh":token, "email": email, "data": Date.now() + 30*24*60*60*1000}
             )
@@ -39,28 +39,30 @@ class userController {
     }
 
     async logout(email){
-        db.update_token_user(email, 'logout')
+        db.update_token_user(email, 'logout') // удаление токена из бд
     }
     
     async getData (){
-        return await dataInfo.getInfo()
+        return await dataInfo.getInfo() // получение информации о системе
     }
+
     async setData(data){
-        const info = await dataInfo.setInfo(data.obj)
+        const info = await dataInfo.setInfo(data.obj) // получение информации о сервисах
         return info
     }
+
     async pushService(name){
-        await db.pushService(name)
+        await db.pushService(name) // добавить сервис в мониторинг
     }
 
     async deleteService(name){
-        await db.deleteService(name)
+        await db.deleteService(name) // удалить сервис из мониторинга
     }
     async getServices(name){
         const result = []
-        const dbresult = await db.getServices()
+        const dbresult = await db.getServices() // запрашиваем сервисы из бд
         for (let item of dbresult){
-            const status =  await dataInfo.setInfo(item.name)
+            const status =  await dataInfo.setInfo(item.name) // получаем статус сервиса 
             result.push({"name": item.name, "status": status})
         }
         return result
